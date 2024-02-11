92cdb110d91b9faa114b8e9808d4b4843d1bda22
Alvaro R Martin <exagonsoft@gmail.com>
2024-02-11 19:12:05 +0100

    #2 App Colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  final Color textColor = const Color(0xFFccc7c5);
+  final Color primaryColor = const Color.fromARGB(255, 39, 209, 33);
+  final Color secondaryColor = const Color.fromARGB(255, 91, 44, 199);
+}
