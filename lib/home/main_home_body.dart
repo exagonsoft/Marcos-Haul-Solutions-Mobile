aef115c8f75285ac3cae85d40c603e8a0aa40bdb
Alvaro R Martin <exagonsoft@gmail.com>
2024-02-11 21:12:55 +0100

    #2 Create Home Body

---
+++ lib/home/main_home_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainHomeBody extends StatefulWidget {
+  const MainHomeBody({super.key});
+
+  @override
+  State<MainHomeBody> createState() => _MainHomeBodyState();
+}
+
+class _MainHomeBodyState extends State<MainHomeBody> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
