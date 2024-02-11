f67ed1f351a3b503251b93c0e580ae1864564b5e
Alvaro R Martin <exagonsoft@gmail.com>
2024-02-12 00:39:29 +0100

    #4 Add Images

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 class AppColors {
-  static const Color textColor = Color(0xFFccc7c5);
+  static const Color textColor = Color.fromARGB(255, 87, 84, 83);
   static const Color blackTextColor = Color.fromARGB(255, 14, 13, 13);
   static const Color primaryColor = Color.fromARGB(255, 39, 209, 33);
   static const Color secondaryColor = Color.fromARGB(255, 91, 44, 199);
