da0978e426f1a4456ccbba27d25cc27007c6b715
Alvaro R Martin <exagonsoft@gmail.com>
2024-02-12 02:46:51 +0100

    #6 Add languages file

---
+++ lib/langs/langs.dart
@@ -0,0 +1,14 @@
+class AppLangs {
+  static Map<String, dynamic> langs = {
+    'en': {
+      'homePageUI': {
+        'title': 'Most Recent',
+      },
+    },
+    'es': {
+      'homePageUI': {
+        'title': 'Mas Reciente',
+      },
+    },
+  };
+}
